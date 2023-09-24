83f5e0c4350252e19a0d038d5286d4989a2330e2
srinibas khuntia <srinibas.k@pillir.io>
2023-09-24 23:48:05 +0530

    throwing Exception

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,8 @@ import java.text.SimpleDateFormat;
 public class Main {
     public static void main(String[] args) {
 
+        ExceptionDemo.show();
+
        //  ExceptionDemo.show("hello");
 
         //FileReader reader= null;
@@ -17,36 +19,36 @@ public class Main {
         //catching exception
 
 
-        try(
-                var reader = new FileReader("file.txt");
-                var writer = new FileWriter("Hello file");
-        ){
-            //reader = new FileReader("file.txt");
-           reader.read();
-           var data = new SimpleDateFormat().parse("");
-
-            System.out.println("Read File");
-        }
-//        catch(FileNotFoundException e){
+//        try(
+//                var reader = new FileReader("file.txt");
+//                var writer = new FileWriter("Hello file");
+//        ){
+//            //reader = new FileReader("file.txt");
+//           reader.read();
+//           var data = new SimpleDateFormat().parse("");
+//
+//            System.out.println("Read File");
+//        }
+////        catch(FileNotFoundException e){
+////            System.out.println(e.getMessage());
+////        }
+//        catch (IOException | ParseException e){
 //            System.out.println(e.getMessage());
 //        }
-        catch (IOException | ParseException e){
-            System.out.println(e.getMessage());
-        }
-//        catch(ParseException e){
-//            System.out.println(e.getMessage());
-//        }
-        //sometimes we dont have to write finally wehn we are handling external reaources in try block
-//        finally {
-//            try{
-//                if(reader!=null){
-//                    reader.close();
-//                }
-//            }
-//            catch (IOException e){
-//                e.printStackTrace();
-//            }
-//        }
+////        catch(ParseException e){
+////            System.out.println(e.getMessage());
+////        }
+//        //sometimes we dont have to write finally wehn we are handling external reaources in try block
+////        finally {
+////            try{
+////                if(reader!=null){
+////                    reader.close();
+////                }
+////            }
+////            catch (IOException e){
+////                e.printStackTrace();
+////            }
+////        }
 
 
     }
--- src/exceptions/ExceptionDemo.java
+++ src/exceptions/ExceptionDemo.java
@@ -1,10 +1,20 @@
 package exceptions;
 
+import java.io.IOException;
+
 public class ExceptionDemo {
 
-    public static void show(String name){
-        sayHello(null);;
+    public static void show(){
+        //sayHello(null);;
+        var account = new Account();
+        try{
+            account.deposit(-1);
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
     }
+
     public static void sayHello(String name){
         System.out.println(name.toUpperCase());
     }
