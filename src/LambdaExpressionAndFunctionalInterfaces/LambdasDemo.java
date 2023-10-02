1efb5bf3510199d6604f113824bdb4a9a97c0f88
srinibas khuntia <srinibas.k@pillir.io>
2023-10-02 22:42:03 +0530

    lambda expression

--- src/LambdaExpressionAndFunctionalInterfaces/LambdasDemo.java
+++ src/LambdaExpressionAndFunctionalInterfaces/LambdasDemo.java
@@ -4,7 +4,24 @@ public class LambdasDemo {
 
     public static  void show(){
         //dependecy injection
-        greet(new ConsolePrinter());
+        //greet(new ConsolePrinter());
+
+        //anonymous inner class
+//        greet(new Printer() {
+//            @Override
+//            public void print(String message) {
+//                System.out.println("Printed "+message);
+//            }
+//        });
+
+//        improvement over anonymous lambda expression
+
+//        greet((String message) ->{
+//            System.out.println("Pinted "+ message);
+//        });
+
+//        more simplification of lambda expression
+        greet(message -> System.out.println("Printed "+message));
     }
 
 
