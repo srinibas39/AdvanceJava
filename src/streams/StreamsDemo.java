5616ce33ac646142563b941974b304d1d06fae5c
srinibas khuntia <srinibaskhuntia39@gmail.com>
2023-10-22 05:51:02 -0700

    Distinct elements

--- src/streams/StreamsDemo.java
+++ src/streams/StreamsDemo.java
@@ -108,43 +108,59 @@ public class StreamsDemo {
 
         //slicing methods
 
+//        var movies = List.of(
+//                new Movie("movie1",10),
+//                new Movie("movie2",20),
+//                new Movie("movie3",30),
+//                new Movie("movie4",40)
+//        );
+//
+//        //limit
+//
+//        movies.stream()
+//                .limit(2)
+//                .forEach(movie -> System.out.println(movie.getTitle()));
+//
+//        System.out.println("-------------------------------------------------");
+//
+//        //skip
+//        movies.stream()
+//                .skip(2)
+//                .forEach(movie -> System.out.println(movie.getTitle()));
+//
+//        //Pagination(IQ)
+//        //suppose 1000
+//        //1 page -> 10 movies
+//        //page 3 -> 20 movies skip and 10 movies show
+//        //genral formula; --> skip((page-1) * no of movies) in one page and limit(no of movies in one page)
+//
+//        //TakeWhilw
+//        System.out.println("take while");
+//        movies.stream()
+//                .takeWhile(movie -> movie.getLikes()<= 20)
+//                .forEach(movie -> System.out.println(movie.getTitle()));
+//
+//        //dropWhile
+//        System.out.println("drop while");
+//        movies.stream()
+//                .dropWhile((movie -> movie.getLikes()<=20))
+//                .forEach(movie -> System.out.println(movie.getTitle()));
+
+
+        //Distinct method
+
         var movies = List.of(
-                new Movie("movie1",10),
-                new Movie("movie2",20),
-                new Movie("movie3",30),
-                new Movie("movie4",40)
+                new Movie("m1",10),
+                new Movie("m2",20),
+                new Movie("m3",30),
+                new Movie("m4",40),
+                new Movie("m6",20)
         );
 
-        //limit
-
         movies.stream()
-                .limit(2)
-                .forEach(movie -> System.out.println(movie.getTitle()));
-
-        System.out.println("-------------------------------------------------");
-
-        //skip
-        movies.stream()
-                .skip(2)
-                .forEach(movie -> System.out.println(movie.getTitle()));
-
-        //Pagination(IQ)
-        //suppose 1000
-        //1 page -> 10 movies
-        //page 3 -> 20 movies skip and 10 movies show
-        //genral formula; --> skip((page-1) * no of movies) in one page and limit(no of movies in one page)
-
-        //TakeWhilw
-        System.out.println("take while");
-        movies.stream()
-                .takeWhile(movie -> movie.getLikes()<= 20)
-                .forEach(movie -> System.out.println(movie.getTitle()));
-
-        //dropWhile
-        System.out.println("drop while");
-        movies.stream()
-                .dropWhile((movie -> movie.getLikes()<=20))
-                .forEach(movie -> System.out.println(movie.getTitle()));
+                .map(Movie::getLikes)
+                .distinct()
+                .forEach(System.out::println);
 
 
 
