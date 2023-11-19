d337d47a1b088150e3431e5d3974bb7d305771e5
srinibas khuntia <srinibaskhuntia39@gmail.com>
2023-11-19 07:45:20 -0800

    processes and threads

---
+++ src/concurrencyAndMultiThreading/Threads.java
@@ -0,0 +1,8 @@
+package concurrencyAndMultiThreading;
+
+public class Threads {
+    public static void main(String[] args) {
+        System.out.println(Thread.activeCount());
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
