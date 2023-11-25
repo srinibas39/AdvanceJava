18698d00a140aa5caa9e300d4dab9e5d03a993a2
srinibas khuntia <srinibaskhuntia39@gmail.com>
2023-11-25 01:08:57 -0800

    pausing a thread

--- src/concurrencyAndMultiThreading/DownloadFile.java
+++ src/concurrencyAndMultiThreading/DownloadFile.java
@@ -4,5 +4,15 @@ public class DownloadFile implements Runnable{
     @Override
     public void run() {
         System.out.println("Downloading File "+Thread.currentThread().getName());
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Download Complete"+ Thread.currentThread().getName());
     }
+
+
 }
